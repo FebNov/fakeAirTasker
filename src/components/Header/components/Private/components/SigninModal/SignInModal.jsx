8878b527e776ce9b3cb4e1ca3e698f592bc7a594
Hunterprey <595129026@qq.com>
2020-10-02 01:02:24 +1000

    add SignIn

--- src/components/Header/components/Private/components/SigninModal/SignInModal.jsx
+++ src/components/Header/components/Private/components/SigninModal/SignInModal.jsx
@@ -10,34 +10,161 @@ const Form = styled.div`
   padding: 16px 0;
 `;
 
+const FORM = {
+  email: {
+    label: "Email",
+    type: "text",
+    getErrorMessage: (value) => {
+      if (!value) {
+        return "Please enter your email address";
+      }
+      return "";
+    },
+  },
+  password: {
+    key: "password",
+    label: "Password",
+    type: "password",
+    getErrorMessage: (value) => {
+      if (!value) {
+        return "Please enter your password";
+      }
 
-const SignInModal = ({ onClose, onSignUp }) => (
-  <Modal onClose={onClose}>
-    <Modal.Header>Sign-In</Modal.Header>
+      return "";
+    },
+  },
+};
 
-    <Modal.Body>
-      <Form>
-        <FormItem htmlFor="email" label="Email">
-          <Input id="email" />
-        </FormItem>
-        <FormItem htmlFor="password" label="Password">
-          <Input id="password" type="password" />
-        </FormItem>
-        <FormItem>
-          <Button width="100%" variant="success">
-            Sign In
-          </Button>
-        </FormItem>
-      </Form>
-    </Modal.Body>
-    <Modal.Footer>
-      Not a member yet?&nbsp;
-      <NakedButton variant="link" onClick={onSignUp}>
-        Sign-Up
-      </NakedButton>
-    </Modal.Footer>
-  </Modal>
-);
+class SignInModal extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      formData: {
+        email: {
+          value: "",
+          touched: false,
+        },
+        password: {
+          value: "",
+          touched: false,
+        },
+      },
+    };
+
+    this.handleFormDataChange = this.handleFormDataChange.bind(this);
+    this.handleFormSubmit = this.handleFormSubmit.bind(this);
+  }
+  getErrorMessage(target) {
+    const { formData } = this.state;
+    const data = Object.keys(formData).reduce(
+      (obj, key) => ({
+        ...obj,
+        [key]: formData[key].value,
+      }),
+      {}
+    );
+
+    return FORM[target].getErrorMessage(formData[target].value, data);
+  }
+
+  handleFormDataChange(target) {
+    return (event) => {
+      event.preventDefault();
+      const { value } = event.target;
+
+      this.setState((prevState) => ({
+        formData: {
+          ...prevState.formData,
+          [target]: {
+            value,
+            touched: true,
+          },
+        },
+      }));
+    };
+  }
+
+  handleFormSubmit(event) {
+    const { formData } = this.state;
+
+    event.preventDefault();
+
+    if (!this.isFormValid()) {
+      console.log("There are validation errors");
+
+      return;
+    }
+
+    console.log("Sign up...", formData);
+  }
+  isFormValid() {
+    const { formData } = this.state;
+
+    const errorMessages = Object.keys(formData)
+      .map((key) => {
+        const errorMessage = this.getErrorMessage(key);
+
+        return errorMessage;
+      })
+      .filter((v) => !!v);
+
+    return !errorMessages.length;
+  }
+  render() {
+    const { onClose, onSignUp } = this.props;
+    const { formData } = this.state;
+    return (
+      <Modal onClose={onClose}>
+        <Modal.Header>Sign Up</Modal.Header>
+        <Modal.Body>
+          <Form onSubmit={this.handleFormSubmit}>
+            {Object.keys(FORM).map((key) => {
+              const { label, type } = FORM[key];
+
+              const { value, touched } = formData[key];
+
+              const errorMessage = touched ? this.getErrorMessage(key) : "";
+
+              return (
+                <FormItem
+                  key={key}
+                  htmlFor={key}
+                  label={label}
+                  errorMessage={errorMessage}
+                >
+                  <Input
+                    id={key}
+                    type={type}
+                    error={errorMessage}
+                    value={value}
+                    onChange={this.handleFormDataChange(key)}
+                  />
+                </FormItem>
+              );
+            })}
+            <FormItem>
+              <Button
+                disabled={!this.isFormValid()}
+                width="100%"
+                variant="success"
+              >
+                Sign In
+              </Button>
+            </FormItem>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          Not Member Yet?&nbsp;
+          <NakedButton variant="link" onClick={onSignUp}>
+            Sign Up now
+          </NakedButton>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
+}
+
 SignInModal.propType = {
   onClose: PropTypes.func.isRequired,
   onSignUp: PropTypes.func.isRequired,
